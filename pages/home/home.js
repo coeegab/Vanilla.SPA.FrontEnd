785d644a81f9db88cad2afa6066a60c560c48366
coeegab <eltongabrielofc@gmail.com>
2023-11-28 20:17:58 -0300

    2023-11-28

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,17 @@
 function myHome() {
     changeTitle();
+    console.log('home funciona');
+
+    getAllItems();
+
 }
+
+function getAllItems() {
+    
+    console.log("funciona");
+    return false;
+
+
+}
+
 $(document).ready(myHome);
